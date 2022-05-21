7aae5530cf330effff2cfe07f10d2cbc0756152d
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2022-05-21 10:52:21 -0500

    ### get date/time

--- demo.sh
+++ demo.sh
@@ -9,3 +9,19 @@ if [ -z "$1" ]; then
     echo "Usage:  foo.sh (# of files to generate) [transaction date YYYYMMDD] [output folder]"
     exit    
 fi
+
+### get date/time 
+
+if [ $2 == "random" ]; then
+
+  ### put code here to random date
+  NEW_TRANSACTION_DATE="2020"$(random_number 2 12)$(random_number 2 31)
+  #NEW_TRANSACTION_DATE=$(random_number 4 2019)$(random_number 2 12)$(random_number 2 31)
+  NEW_DD2_DATE="DD2^"${NEW_TRANSACTION_DATE:4:2}"^"${NEW_TRANSACTION_DATE:6:2}"^"${NEW_TRANSACTION_DATE:2:2}
+
+else
+
+  NEW_TRANSACTION_DATE=$2
+  NEW_DD2_DATE="DD2^"${NEW_TRANSACTION_DATE:4:2}"^"${NEW_TRANSACTION_DATE:6:2}"^"${NEW_TRANSACTION_DATE:2:2}
+
+fi
