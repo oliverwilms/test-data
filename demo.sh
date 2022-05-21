81499a0f7b2b6d84f9b9eecc153f37a9f0fa9638
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2022-05-21 10:57:02 -0500

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -5,7 +5,7 @@ function random_number() {
   printf "%0$1g" $(( $RANDOM % $2 +1))
 }
 
-if [ -z "$1" ]; then 
+if [ -z "$2" ]; then 
     echo "Usage:  foo.sh (# of files to generate) [transaction date YYYYMMDD] [output folder]"
     exit    
 fi
